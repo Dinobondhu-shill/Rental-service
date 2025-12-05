7cbf60669135ccb3662816576735a30efd521ec9
Dinobondhu-shill <dinu.webdev@gmail.com>
2025-12-05 08:35:03 +0600

    booking controller, route and service create along with create booking api

--- src/config/db.ts
+++ src/config/db.ts
@@ -16,7 +16,7 @@ const initDB = async() =>{
         )
         `)
 
-        await pool.query(`
+    await pool.query(`
             CREATE TABLE IF NOT EXISTS vehicles(
             id SERIAL PRIMARY KEY,
             vehicle_name VARCHAR(300) NOT NULL,
@@ -25,6 +25,20 @@ const initDB = async() =>{
             daily_rent_price INT,
             availability_status TEXT
             )`)
+
+            // RENT END DATE SHOULD BE IMPLEMENT -
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS bookings(
+      id SERIAL PRIMARY KEY,
+      customer_id INT NOT NULL ,
+      vehicle_id INT NOT NULL ,
+      rent_start_date TIMESTAMP DEFAULT NOW(),
+      rent_end_date TIMESTAMP DEFAULT NOW(),
+      total_price INT,
+      status TEXT
+      )  
+        `)
 
         console.log("connected")
 }
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import initDB, { pool } from './config/db';
 import { userRoutes } from './modules/Users/user.routes';
 import config from './config';
 import { vehicleRoutes } from './modules/Vehicles/vehicle.route';
+import { bookingRoutes } from './modules/Bookings/booking.routes';
 
 
 const app = express();
@@ -17,6 +18,7 @@ initDB()
 
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/vehicles', vehicleRoutes)
+app.use('/api/v1/bookings', bookingRoutes)
 
 app.listen(config.port, ()=>{
     console.log(`Server is running on ${config.port}`)
